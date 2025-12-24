019bc85dce83e869efa1fb595f285ef12094cfd5
dkdkwl <dkdkwl@naver.com>
2025-12-24 22:29:20 +0900

    레이아웃 잡고 템플릿에서 배열 만들어서 list에 넘겨주고 map으로 돌림

--- src/component/TodoListTemplate/TodoListTemplate.jsx
+++ src/component/TodoListTemplate/TodoListTemplate.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TodoHeader from '../TodoHeader/TodoHeader'
 import TodoList from '../TodoList/TodoList'
 import TodoInput from '../TodoInput/TodoInput'
 import styles from './TodoListTemplate.module.css'
 
-console.log(TodoHeader)
-
-
 export default function TodoListTemplate() {
+  const [todosList, setTodosList] = useState([
+    {id:1, text:'강의 보기', checked:false},
+    {id:2, text:'카페가기', checked:true},
+    {id:3, text:'청소하기', checked:false},
+  ]);
   return (
     <div className={styles.container}>
       <div className={styles.TodoListTemplate}>
       <TodoHeader></TodoHeader>
-      <TodoList></TodoList>
+      {
+        todosList.map(element => {
+          return (
+            <TodoList key={element.id} id={element.id} text={element.text} checked={element.checked}></TodoList>
+          )
+        })
+      }
+      
       <TodoInput></TodoInput>
       </div>
     </div>
