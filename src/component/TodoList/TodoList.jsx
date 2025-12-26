1c8a6eec789f21284f9755d95764c7dad8cbe77f
dkdkwl <dkdkwl@naver.com>
2025-12-26 12:47:06 +0900

    pros로 전달해서 간단한 모습 만들기

--- src/component/TodoList/TodoList.jsx
+++ src/component/TodoList/TodoList.jsx
@@ -1,22 +1,12 @@
 import React from 'react'
 import styles from './TodoList.module.css'
 
-export default function TodoList() {
+export default function TodoList({id,text,checked}) {
   return (
     <ul className={styles.todoList}>
       <li className={styles.todoItem}>
-        <input type="checkbox" id="todoListitem1" className={styles.checkbox} />
-        <label htmlFor="todoListitem1" className={styles.label}>강의 보기</label>
-        <button type='button' className={styles.deleteButton}>🗑️</button>
-      </li>
-      <li className={styles.todoItem}>
-        <input type="checkbox" id="todoListitem2" className={styles.checkbox} />
-        <label htmlFor="todoListitem2" className={styles.label}>카페가기</label>
-        <button type='button' className={styles.deleteButton}>🗑️</button>
-      </li>
-      <li className={styles.todoItem}>
-        <input type="checkbox" id="todoListitem3" className={styles.checkbox} />
-        <label htmlFor="todoListitem3" className={styles.label}>청소하기</label>
+        <input type="checkbox" id={`todoListitem${id}`} className={styles.checkbox} checked={checked}/>
+        <label htmlFor={`todoListitem${id}`} className={styles.label}>{text}</label>
         <button type='button' className={styles.deleteButton}>🗑️</button>
       </li>
     </ul>
--- src/component/TodoListTemplate/TodoListTemplate.jsx
+++ src/component/TodoListTemplate/TodoListTemplate.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TodoHeader from '../TodoHeader/TodoHeader'
 import TodoList from '../TodoList/TodoList'
 import TodoInput from '../TodoInput/TodoInput'
 import styles from './TodoListTemplate.module.css'
 
-console.log(TodoHeader)
-
-
 export default function TodoListTemplate() {
+  const [todoList,setTodoList] = useState([
+    {id:1, text:'강의보기', checked:false},
+    {id:2, text:'알고리즘 문제풀기', checked:true},
+    {id:3, text:'운동하기', checked:false},
+  ])
   return (
     <div className={styles.container}>
       <div className={styles.TodoListTemplate}>
       <TodoHeader></TodoHeader>
-      <TodoList></TodoList>
+      {
+       todoList.map(item => (
+        <TodoList key={`todoList.${item.id}`} text={item.text} checked={item.checked}></TodoList>
+       ))
+      }
       <TodoInput></TodoInput>
       </div>
     </div>
